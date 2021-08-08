55d2527d5250c2fb5d3a4f07f4cb7b206d447973
dodopontocom <rodolfotiago@gmail.com>
2021-08-07 22:34:05 -0300

    hash blak2b II

--- hash/_cookie.py
+++ hash/_cookie.py
@@ -4,7 +4,7 @@ from hashlib import blake2b
 from hmac import compare_digest
 
 SECRET_KEY = "sometextheretogeneraterandomsecret".encode()
-AUTH_SIZE = 64
+AUTH_SIZE = 32
 
 def sign(cookie):
     h = blake2b(digest_size=AUTH_SIZE, key=SECRET_KEY)
@@ -17,11 +17,13 @@ def verify(cookie, sig):
         
 cookie = "user-alice".encode()
 sig = sign("rodolfo".encode())
-#sig = sign(cookie)
+sig = sign(cookie)
 print("{0},{1}".format(cookie, sig))
 print("{0},{1}".format(cookie.decode('utf-8'), sig))
 #user-alice,b'43b3c982cf697e0c5ab22172d1ca7421'
 print(verify(cookie, sig))
 print(verify(b'user-bob', sig))
-print(verify(cookie, b'0102030405060708090a0b0c0d0e0f00'))
+print(verify(cookie, b'6a9e1f38f09cc8b5a6e8e589f3ad6c3ab2ae36aa1a7a9b8bc11dd7e57728fe8c'))
+
+
 
--- hash/cookie.py
+++ hash/cookie.py
@@ -4,8 +4,10 @@ from hashlib import blake2b
 from hmac import compare_digest
 import json
 import time
+import subprocess
 
-SECRET_KEY = "sometextheretogeneraterandomsecret".encode()
+S_KEY = str(subprocess.getoutput('arch')) + "_" + str(subprocess.getoutput('lsb_release -cs'))
+SECRET_KEY = S_KEY.encode('utf-8')
 AUTH_SIZE = 32
 
 chain = [
@@ -22,33 +24,34 @@ chain = [
 
         ]
 block_chain = []
+
 def get_time():
     return time.time()
 
 def sign(cookie):
     h = blake2b(digest_size=AUTH_SIZE, key=SECRET_KEY)
     h.update(cookie)
-    return h.hexdigest()
+    return_cookie = h.hexdigest()
+    return return_cookie
     
 def verify(cookie, sig):
         good_sig = sign(cookie)
         return compare_digest(good_sig, sig)
         
 for c in chain:
-
-    cookie = json.dumps(c).encode()
+    cookie = json.dumps(c).encode('utf-8')
     sig_cookie = sign(cookie)
-    #print(verify(cookie, sig_cookie))
+    print(verify(cookie, sig_cookie))
 
 def add_block(block):
     if (len(block_chain) == 0):
         block["timestamp"] = get_time()
-        block["hash"] = sign(json.dumps(block).encode())
+        block["hash"] = sign(json.dumps(block).encode('utf-8'))
     else:
         block["timestamp"] = get_time()
         last_block = block_chain[-1]
         block["last_hash"] = last_block["hash"]
-        block["hash"] = sign(json.dumps(block).encode())
+        block["hash"] = sign(json.dumps(block).encode('utf-8'))
     block_chain.append(block)
 
 for t in chain:
@@ -57,6 +60,6 @@ for t in chain:
 print(block_chain)
 
 for v in block_chain:
-    cookie = json.dumps(v).encode()
+    cookie = json.dumps(v).encode('utf-8')
     sig_cookie = sign(cookie)
     print(verify(cookie, sig_cookie))
